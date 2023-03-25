0e9c4c45d33faff184cd606a998358e818b44da0
Lee_Desktop <leejinwook69@gmail.com>
2023-03-26 03:08:49 +0900

    commit1

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import AppRouter from "./Router";
+
+function App() {
+  const[isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+  <>
+  <AppRouter isLoggedIn={isLoggedIn} />;
+  <footer>&copy; Nwitter{new Date().getFullYear()} </footer>
+  
+  </>
+  );
+}
+
+export default App;
